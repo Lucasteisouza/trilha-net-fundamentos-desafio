803b3e686e40f55aad9a2c13f883e30d1d5f7853
Lucasteisouza <lucasteisouza@gmail.com>
2023-10-31 11:59:08 -0300

    adiciona validação de formato de placa para corresponder ao formato AAA000 (padrão brasil) ou AAA0A00 (padrão mercosul

--- DesafioFundamentos/Models/Estacionamento.cs
+++ DesafioFundamentos/Models/Estacionamento.cs
@@ -1,3 +1,5 @@
+using System.Text.RegularExpressions;
+
 namespace DesafioFundamentos.Models
 {
     public class Estacionamento
@@ -14,10 +16,14 @@ namespace DesafioFundamentos.Models
 
         public void AdicionarVeiculo()
         {
-            // TODO: Pedir para o usuário digitar uma placa (ReadLine) e adicionar na lista "veiculos"
-            // *IMPLEMENTE AQUI*
+            Regex regex = new Regex(@"^[a-zA-Z]{3}\d{4}$");
             Console.WriteLine("Digite a placa do veículo para estacionar:");
             string resposta = Console.ReadLine();
+            if (!regex.IsMatch(resposta))
+            {
+                Console.WriteLine("Placa inválida. Digite uma placa no formato AAA0000");
+                return;
+            }
             veiculos.Add(resposta);
             Console.WriteLine($"O veículo {resposta} foi estacionado com sucesso!");
         }
@@ -25,27 +31,15 @@ namespace DesafioFundamentos.Models
         public void RemoverVeiculo()
         {
             Console.WriteLine("Digite a placa do veículo para remover:");
-
-            // Pedir para o usuário digitar a placa e armazenar na variável placa
-            // *IMPLEMENTE AQUI*
             string placa = "";
             placa = Console.ReadLine();
-
-            // Verifica se o veículo existe
             if (veiculos.Any(x => x.ToUpper() == placa.ToUpper()))
             {
                 Console.WriteLine("Digite a quantidade de horas que o veículo permaneceu estacionado:");
-
-                // TODO: Pedir para o usuário digitar a quantidade de horas que o veículo permaneceu estacionado,
-                // TODO: Realizar o seguinte cálculo: "precoInicial + precoPorHora * horas" para a variável valorTotal                
-                // *IMPLEMENTE AQUI*
                 int horas = 0;
                 decimal valorTotal = 0; 
                 horas = Convert.ToInt32(Console.ReadLine());
                 valorTotal = precoInicial + precoPorHora * horas;
-
-                // TODO: Remover a placa digitada da lista de veículos
-                // *IMPLEMENTE AQUI*
 
                 veiculos.Remove(placa);
 
@@ -59,12 +53,10 @@ namespace DesafioFundamentos.Models
 
         public void ListarVeiculos()
         {
-            // Verifica se há veículos no estacionamento
             if (veiculos.Any())
             {
                 Console.WriteLine("Os veículos estacionados são:");
-                // TODO: Realizar um laço de repetição, exibindo os veículos estacionados
-                // *IMPLEMENTE AQUI*
+
                 foreach (var item in veiculos)
                 {
                     Console.WriteLine(item);
